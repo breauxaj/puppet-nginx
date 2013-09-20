fb52a8b887322cb267644d6e2a185d00cb0e0d74
Adam Breaux <breauxaj@gmail.com>
2013-09-20 13:25:20 -0400

    puppet lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,30 +1,17 @@
 class nginx {
-  $required = $operatingsystem ? {
+  $required = $::operatingsystem ? {
     /(?i-mx:centos|fedora|redhat|scientific)/ => 'nginx',
   }
 
-  package { $required: ensure => latest }
-    
   $paths = [ '/etc/nginx', '/etc/nginx/conf.d', '/var/log/nginx' ]
-    
+
+  package { $required: ensure => latest }
+
   file { $paths:
     ensure => directory,
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
-  }
-
-}
-
-define nginx::service ( $ensure = running,
-                        $enable = true ) {
-  $service = $operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => 'nginx',
-  }
-
-  service { $service:
-    ensure => $ensure,
-    enable => $enable,
   }
 
 }
